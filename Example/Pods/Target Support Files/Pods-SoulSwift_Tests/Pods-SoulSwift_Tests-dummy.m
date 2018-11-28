7bbc70e73c2d7e48b5c62fbc61c3846012feaa7f
Evgeny Vandyshev <evgeny.vandyshev@soulplatform.com>
2018-11-28 09:44:44 +0300

    Install pods

---
+++ Example/Pods/Target Support Files/Pods-SoulSwift_Tests/Pods-SoulSwift_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SoulSwift_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SoulSwift_Tests
+@end
---
+++ Example/Pods/Target Support Files/SoulSwift/SoulSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SoulSwift : NSObject
+@end
+@implementation PodsDummy_SoulSwift
+@end

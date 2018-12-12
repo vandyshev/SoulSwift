b323d55660c32fbb25106daaf84e27df8df259c0
Evgeny Vandyshev <evgeny.vandyshev@soulplatform.com>
2018-12-12 15:29:26 +0300

    Add two files for test

---
+++ Sources/Classes/Api/App/SoulAuthServiceImpl2.swift
@@ -0,0 +1,13 @@
+//
+//  SoulAuthServiceImpl2.swift
+//  Pods
+//
+//  Created by Евгений Вандышев on 12/12/2018.
+//
+
+class SoulAppServiceImpl2: SoulAppService {
+    
+    func features(anonymous: Bool, anonymousId: String, completion: () -> Void) {
+        
+    }
+}

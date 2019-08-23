4b4fe12874eec1a2249d2e3faa0de16a04fda8ee
Evgeny Vandyshev <evgeny.vandyshev@soulplatform.com>
2019-08-23 12:04:32 +0300

    Fix autoregistration

--- Sources/Classes/Assemblies/ServicesAssembly.swift
+++ Sources/Classes/Assemblies/ServicesAssembly.swift
@@ -6,6 +6,11 @@ final class ServicesAssembly: Assembly {
         container.autoregister(ApplicationServiceProtocol.self, initializer: ApplicationService.init)
         container.autoregister(AuthServiceProtocol.self, initializer: AuthService.init)
         container.autoregister(MeServiceProtocol.self, initializer: MeService.init)
+        container.autoregister(UsersServiceProtocol.self, initializer: UsersService.init)
+        container.autoregister(ChatsServiceProtocol.self, initializer: ChatsService.init)
+        container.autoregister(EventsServiceProtocol.self, initializer: EventsService.init)
+        container.autoregister(PurchasesServiceProtocol.self, initializer: PurchasesService.init)
+        container.autoregister(BlocksServiceProtocol.self, initializer: BlocksService.init)
         container.autoregister(StorageServiceProtocol.self, initializer: StorageService.init)
     }
 }

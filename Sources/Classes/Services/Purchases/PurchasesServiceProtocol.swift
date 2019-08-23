b794c845e19ca70e507acb818e1b63719fab0697
Evgeny Vandyshev <evgeny.vandyshev@soulplatform.com>
2019-08-23 13:53:06 +0300

    Fix incorrect service

--- Sources/Classes/Services/Purchases/PurchasesServiceProtocol.swift
+++ Sources/Classes/Services/Purchases/PurchasesServiceProtocol.swift
@@ -1,5 +1,7 @@
 public protocol PurchasesServiceProtocol {
-//    func getMe(completion: @escaping (Result<MyUser, SoulSwiftError>) -> Void)
+    func allPurchases(completion: @escaping (Result<[SoulBundle], SoulSwiftError>) -> Void)
+    func myPurchases(completion: @escaping (Result<([SoulBundle], [InventoryItem]), SoulSwiftError>) -> Void)
+    func orderAppstore(receipt: String, completion: @escaping (Result<([SoulBundle], [InventoryItem]), SoulSwiftError>) -> Void)
 }
 
 final class PurchasesService: PurchasesServiceProtocol {
@@ -8,5 +10,37 @@ final class PurchasesService: PurchasesServiceProtocol {
 
     init(soulProvider: SoulProviderProtocol) {
         self.soulProvider = soulProvider
+    }
+
+    func allPurchases(completion: @escaping (Result<[SoulBundle], SoulSwiftError>) -> Void) {
+        let request = SoulRequest(
+            soulEndpoint: SoulPurchasesEndpoint.all,
+            needAuthorization: true
+        )
+        soulProvider.request(request) { (result: Result<SoulResponse, SoulSwiftError>) in
+            completion(result.map { $0.bundles })
+        }
+    }
+
+    func myPurchases(completion: @escaping (Result<([SoulBundle], [InventoryItem]), SoulSwiftError>) -> Void) {
+        let request = SoulRequest(
+            soulEndpoint: SoulPurchasesEndpoint.my,
+            needAuthorization: true
+        )
+        soulProvider.request(request) { (result: Result<SoulResponse, SoulSwiftError>) in
+            completion(result.map { ($0.bundles, $0.items) })
+        }
+    }
+
+    func orderAppstore(receipt: String, completion: @escaping (Result<([SoulBundle], [InventoryItem]), SoulSwiftError>) -> Void) {
+        let request = SoulRequest(
+            httpMethod: .POST,
+            soulEndpoint: SoulPurchasesEndpoint.orderAppstore,
+            bodyParameters: ["receipt": receipt],
+            needAuthorization: true
+        )
+        soulProvider.request(request) { (result: Result<SoulResponse, SoulSwiftError>) in
+            completion(result.map { ($0.bundles, $0.items) })
+        }
     }
 }
--- Sources/Classes/Services/Users/UsersServiceProtocol.swift
+++ Sources/Classes/Services/Users/UsersServiceProtocol.swift
@@ -1,7 +1,4 @@
 public protocol UsersServiceProtocol {
-    func allPurchases(completion: @escaping (Result<[SoulBundle], SoulSwiftError>) -> Void)
-    func myPurchases(completion: @escaping (Result<([SoulBundle], [InventoryItem]), SoulSwiftError>) -> Void)
-    func orderAppstore(receipt: String, completion: @escaping (Result<([SoulBundle], [InventoryItem]), SoulSwiftError>) -> Void)
 }
 
 final class UsersService: UsersServiceProtocol {
@@ -10,37 +7,5 @@ final class UsersService: UsersServiceProtocol {
 
     init(soulProvider: SoulProviderProtocol) {
         self.soulProvider = soulProvider
-    }
-
-    func allPurchases(completion: @escaping (Result<[SoulBundle], SoulSwiftError>) -> Void) {
-        let request = SoulRequest(
-            soulEndpoint: SoulPurchasesEndpoint.all,
-            needAuthorization: true
-        )
-        soulProvider.request(request) { (result: Result<SoulResponse, SoulSwiftError>) in
-            completion(result.map { $0.bundles })
-        }
-    }
-
-    func myPurchases(completion: @escaping (Result<([SoulBundle], [InventoryItem]), SoulSwiftError>) -> Void) {
-        let request = SoulRequest(
-            soulEndpoint: SoulPurchasesEndpoint.my,
-            needAuthorization: true
-        )
-        soulProvider.request(request) { (result: Result<SoulResponse, SoulSwiftError>) in
-            completion(result.map { ($0.bundles, $0.items) })
-        }
-    }
-
-    func orderAppstore(receipt: String, completion: @escaping (Result<([SoulBundle], [InventoryItem]), SoulSwiftError>) -> Void) {
-        let request = SoulRequest(
-            httpMethod: .POST,
-            soulEndpoint: SoulPurchasesEndpoint.orderAppstore,
-            bodyParameters: ["receipt": receipt],
-            needAuthorization: true
-        )
-        soulProvider.request(request) { (result: Result<SoulResponse, SoulSwiftError>) in
-            completion(result.map { ($0.bundles, $0.items) })
-        }
     }
 }

8de215caec22a6600bbc709483a8a11f7074393c
Evgeny Vandyshev <evgeny.vandyshev@soulplatform.com>
2019-11-19 21:27:28 +0300

    Setup ContactRequest

--- Sources/Soul/Classes/Api/Models/Responses/ContactRequest.swift
+++ Sources/Soul/Classes/Api/Models/Responses/ContactRequest.swift
@@ -2,22 +2,22 @@ import Foundation
 
 public struct ContactRequest {
 
-    enum Status: String, Codable {
+    public enum Status: String, Codable {
         case sent, approved, declined, canceled
-        var isSent: Bool {
+        public var isSent: Bool {
             guard case .sent = self else { return false }
             return true
         }
-        var isDeclined: Bool {
+        public var isDeclined: Bool {
             guard case .declined = self else { return false }
             return true
         }
     }
 
-    let identifier: String
-    let toUser: String
-    let fromUser: String
-    var status: Status
+    public let identifier: String
+    public let toUser: String
+    public let fromUser: String
+    public var status: Status
 }
 
 extension ContactRequest: Codable {
@@ -29,10 +29,10 @@ extension ContactRequest: Codable {
     }
 }
 
-struct MessageContactInfo {
-    let userId: String
-    let nickname: String
-    let request: ContactRequest
+public struct MessageContactInfo {
+    public let userId: String
+    public let nickname: String
+    public let request: ContactRequest
 }
 
 extension MessageContactInfo: Codable {
